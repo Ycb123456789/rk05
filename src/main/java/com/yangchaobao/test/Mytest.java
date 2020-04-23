b85fe65b4feb8de01d1f614a32fe42e62e5822b8
ycb <yangchaobao@qq.com>
2020-04-23 09:41:15 +0800

    aasdf

--- src/main/java/com/yangchaobao/test/Mytest.java
+++ src/main/java/com/yangchaobao/test/Mytest.java
@@ -2,6 +2,7 @@ package com.yangchaobao.test;
 
 public class Mytest {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world111");
+		
 	}
 }
